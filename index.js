9a5445eecfd4d32edd014e8de8db0e8ee3ff3f11
Milan Rusev <15890495+MilanRusev@users.noreply.github.com>
2018-12-06 18:33:48 +0200

    Global primary key

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ const PARITY_NODE = process.env.PARITY_URL || "http://localhost:8545/";
 console.info(`Connecting to parity node ${PARITY_NODE}`)
 let web3 = new Web3(new Web3.providers.HttpProvider(PARITY_NODE))
 
-let lastProcessedBlock = parseInt(process.env.START_BLOCK || "0")
+let lastProcessedPosition = {
+  blockNumber: parseInt(process.env.START_BLOCK || "-1"),
+  primaryKey: parseInt(process.env.START_PRIMARY_KEY || "-1")
+}
 
 async function getBlockTimestamp(blockNumber) {
   const block = await web3.eth.getBlock(blockNumber)
@@ -34,7 +37,7 @@ async function decodeEvent(event, blockTimestamps) {
     timestamp = blockTimestamps[event["blockNumber"]]
   }
 
-  return JSON.stringify({
+  return {
     from: decodeAddress(event["topics"][1]),
     to: decodeAddress(event["topics"][2]),
     value: parseFloat(web3.utils.hexToNumberString(event["data"])),
@@ -44,7 +47,7 @@ async function decodeEvent(event, blockTimestamps) {
     timestamp: timestamp,
     transactionHash: event["transactionHash"],
     logIndex: parseInt(web3.utils.hexToNumberString(event["logIndex"]))
-  })
+  }
 }
 
 async function getPastEvents(fromBlock, toBlock) {
@@ -72,26 +75,33 @@ async function getPastEvents(fromBlock, toBlock) {
 
 async function work() {
   const currentBlock = await web3.eth.getBlockNumber() - CONFIRMATIONS
-  console.info(`Fetching transfer events for interval ${lastProcessedBlock}:${currentBlock}`)
+  console.info(`Fetching transfer events for interval ${lastProcessedPosition.blockNumber}:${currentBlock}`)
 
-  while (lastProcessedBlock < currentBlock) {
-    const toBlock = Math.min(lastProcessedBlock + BLOCK_INTERVAL, currentBlock)
-    const events = await getPastEvents(lastProcessedBlock + 1, toBlock)
+  while (lastProcessedPosition.blockNumber < currentBlock) {
+    const toBlock = Math.min(lastProcessedPosition.blockNumber + BLOCK_INTERVAL, currentBlock)
+    const events = await getPastEvents(lastProcessedPosition.blockNumber + 1, toBlock)
 
     if (events.length > 0) {
-      console.info(`Storing ${events.length} messages for blocks ${lastProcessedBlock + 1}:${toBlock}`)
-      await exporter.sendData(events)
+      for(let i = 0; i < events.length; i++) {
+        events[i].primaryKey = lastProcessedPosition.primaryKey + i + 1
+      }
+
+      console.info(`Storing and setting primary keys ${events.length} messages for blocks ${lastProcessedPosition.blockNumber + 1}:${toBlock}`)
+      
+      await exporter.sendDataWithKey(events, "primaryKey")
+
+      lastProcessedPosition.primaryKey += events.length
     }
 
-    lastProcessedBlock = toBlock
-    await exporter.savePosition(lastProcessedBlock)
+    lastProcessedPosition.blockNumber = toBlock
+    await exporter.savePosition(lastProcessedPosition)
   }
 }
 
 async function fetchEvents() {
   await work()
   .then(() => {
-    console.log(`Progressed to block ${lastProcessedBlock}`)
+    console.log(`Progressed to position ${JSON.stringify(lastProcessedPosition)}`)
 
     // Look for new events every 30 sec
     setTimeout(fetchEvents, 30 * 1000)
@@ -102,11 +112,11 @@ async function initLastProcessedBlock() {
   const lastPosition = await exporter.getLastPosition()
 
   if (lastPosition) {
-    lastProcessedBlock = lastPosition
-    console.info(`Resuming export from block ${lastPosition}`)
+    lastProcessedPosition = lastPosition
+    console.info(`Resuming export from position ${JSON.stringify(lastPosition)}`)
   } else {
-    await exporter.savePosition(lastProcessedBlock)
-    console.info(`Initialized exporter with initial block ${lastProcessedBlock}`)
+    await exporter.savePosition(lastProcessedPosition)
+    console.info(`Initialized exporter with initial position ${JSON.stringify(lastProcessedPosition)}`)
   }
 }
 
