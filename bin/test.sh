0d9c9c14eb1a7d35184169993bfef6177b9bd4bd
Yordan Pavlov <y.d.pavlov@gmail.com>
2021-01-14 15:29:44 +0200

    Contract mapping json file is provided for testing.

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build --build-arg NODE_ENV=development -t erc20-transfers-exporter-test -f docker/Dockerfile . &&
-docker run --rm -t erc20-transfers-exporter-test npm test
+docker-compose -f ./docker/docker-compose-test.yaml build && \
+docker-compose -f ./docker/docker-compose-test.yaml run test
