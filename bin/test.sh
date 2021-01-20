f6790c79707503f1d9c16d9a817da21e483fba98
Yordan Pavlov <y.d.pavlov@gmail.com>
2021-01-20 14:12:56 +0200

    More debug logging.

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
 docker build --build-arg NODE_ENV=development -t erc20-transfers-exporter-test -f docker/Dockerfile .
+echo "Docker run is run from $(pwd)"
 docker run  -v $(pwd)/test/contract_mapping:/opt/app/lib/contract_mapping/ -t erc20-transfers-exporter-test npm test 
