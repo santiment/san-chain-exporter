b4e2f659d723ec69ec1e1c3d7bd72ad3c25c548d
Yordan Pavlov <y.d.pavlov@gmail.com>
2021-01-20 14:56:01 +0200

    Switched test to using a separate Dockerfile.
    * I could not work around Jenkins not finding volumes to mount for the
      test.
    * Switching to separate Dockefile where the JSON file needed for the
      test is explicitly copied inside.

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
 
-docker build --build-arg NODE_ENV=development -t erc20-transfers-exporter-test -f docker/Dockerfile .
-echo "Docker run is run from $(pwd)"
-docker run  -v $(pwd)/test/contract_mapping:/opt/app/lib/contract_mapping/ -t erc20-transfers-exporter-test npm test 
+docker build --build-arg NODE_ENV=development -t erc20-transfers-exporter-test -f docker/Dockerfile-test .
+docker run --rm -t erc20-transfers-exporter-test
