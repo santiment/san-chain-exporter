22013dccc1d525e873613641f34e405c362f3a65
Yordan Pavlov <y.d.pavlov@gmail.com>
2021-11-03 09:45:56 +0200

    Test for overwritten event ordering

--- test/erc20/worker.spec.js
+++ test/erc20/worker.spec.js
@@ -7,10 +7,12 @@ const extend_events = require('./extend_events.spec')
 
 describe('Test ERC20 worker', function() {
     let originalEvent = null
+    let originalEvent2 = null
     let originalEventWithPrimaryKey = null
     let correctedEvent = null
     let correctedEventWithPrimaryKey = null
     const CONTRACT_ORIGINAL = '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f'
+    const NOT_OVERWRITTEN_CONTRACT = '0xd022b63ee8576fb46f5e1c5751ca3b9fe0af2a6f'
     const CONTRACT_REPLACE = 'snx_contract'
 
     beforeEach(function() {
@@ -25,6 +27,18 @@ describe('Test ERC20 worker', function() {
         "value": 1.81e+21,
         "valueExactBase36": "alzj4rdbzkcq9s"
         }
+
+        originalEvent2 = {
+            "contract": NOT_OVERWRITTEN_CONTRACT,
+            "blockNumber": 10449854,
+            "timestamp": 0,
+            "transactionHash": "0x246616c3cf211facc802a1f659f64cefe7b6f9be50da1908fcea23625e97d1cb",
+            "logIndex": 158,
+            "to": "0x3f5ce5fbfe3e9af3971dd833d26ba9b5c936f0be",
+            "from": "0xea5f6f8167a60f671cc02b074b6ac581153472c9",
+            "value": 1.81e+21,
+            "valueExactBase36": "alzj4rdbzkcq9s"
+            }
 
         correctedEvent = JSON.parse(JSON.stringify(originalEvent))
         correctedEvent.contract = CONTRACT_REPLACE
@@ -75,12 +89,10 @@ describe('Test ERC20 worker', function() {
         // Overwrite variables and methods that the 'work' method would use internally.
         erc20_worker.__set__("constants", {CONTRACT_MODE: "extract_all_append"})
         contract_overwrite.__set__("constants", {CONTRACT_MODE: "extract_all_append"})
-        const contractEditor = contract_overwrite.contractEditor
-        contractEditor.getPastEvents = async function () {
+        erc20_worker.__set__("getPastEvents", async function () {
             return [originalEvent]
-        }
+        })
 
-        erc20_worker.__set__("contractEditor", contractEditor)
         const worker = new erc20_worker.worker();
         worker.lastConfirmedBlock = 1
         worker.lastExportedBlock = 0
@@ -90,5 +102,24 @@ describe('Test ERC20 worker', function() {
         const result = await worker.work()
 
         assert.deepStrictEqual(result, [originalEvent, correctedEventWithPrimaryKey])
+    })
+
+    it("test multiple events returned when in 'extract_all_append' mode", async function () {
+        // Test that the overwritten event would be correctly ordered in between two original events
+        erc20_worker.__set__("constants", {CONTRACT_MODE: "extract_all_append"})
+        contract_overwrite.__set__("constants", {CONTRACT_MODE: "extract_all_append"})
+        erc20_worker.__set__("getPastEvents", async function () {
+            return [originalEvent, originalEvent2]
+        })
+
+        const worker = new erc20_worker.worker();
+        worker.lastConfirmedBlock = 1
+        worker.lastExportedBlock = 0
+
+        // In this mode the primary key is 1 more than the 'original' event
+        correctedEventWithPrimaryKey.primaryKey +=  1
+        const result = await worker.work()
+
+        assert.deepStrictEqual(result, [originalEvent, correctedEventWithPrimaryKey, originalEvent2])
     })
 })
