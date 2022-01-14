0f93cb16c8e0e59d9e344d48bb8284a9544355cb
antisaa <antesosa95@gmail.com>
2022-01-14 10:13:52 +0100

    Increase the number of confirmations for matic

--- blockchains/matic/lib/constants.js
+++ blockchains/matic/lib/constants.js
@@ -1,5 +1,5 @@
 const BLOCK_INTERVAL = parseInt(process.env.BLOCK_INTERVAL || "100")
-const CONFIRMATIONS = parseInt(process.env.CONFIRMATIONS || "3")
+const CONFIRMATIONS = parseInt(process.env.CONFIRMATIONS || "15")
 const PARITY_NODE = process.env.PARITY_URL || "http://localhost:8545/"
 const LOOP_INTERVAL_CURRENT_MODE_SEC = parseInt(process.env.LOOP_INTERVAL_CURRENT_MODE_SEC || "30")
 
