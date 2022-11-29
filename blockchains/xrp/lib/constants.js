24cdbedb44b5c1c96f793debf0a675a2991feb4c
Lyudmil Danailov <spiderjako@Lyudmils-MacBook-Pro.local>
2022-11-29 18:32:26 +0200

    Adding XRP blockchain exporter

---
+++ blockchains/xrp/lib/constants.js
@@ -0,0 +1,24 @@
+// Although we request the last 'validated' block we are seeing blocks which are neither validated nor even closed.
+// We introduce this extra delay to prevent this.
+// In terms of time, 20 blocks is a delay between 1 and 2 minutes.
+const CONFIRMATIONS = parseInt(process.env.CONFIRMATIONS || '20');
+const SEND_BATCH_SIZE = parseInt(process.env.SEND_BATCH_SIZE || '30');
+const DEFAULT_WS_TIMEOUT = parseInt(process.env.DEFAULT_WS_TIMEOUT || '10000');
+const CONNECTIONS_COUNT = parseInt(process.env.CONNECTIONS_COUNT || '1');
+const MAX_CONNECTION_CONCURRENCY = parseInt(process.env.MAX_CONNECTION_CONCURRENCY || '10');
+const XRP_NODE_URLS = process.env.XRP_NODE_URLS || 'wss://s2.ripple.com';
+const EXPORT_TIMEOUT_MLS = parseInt(process.env.EXPORT_TIMEOUT_MLS || 1000 * 60 * 5);
+const START_BLOCK = parseInt(process.env.START_BLOCK || '38129');
+const LOOP_INTERVAL_CURRENT_MODE_SEC = 1000;
+
+module.exports = {
+  SEND_BATCH_SIZE,
+  DEFAULT_WS_TIMEOUT,
+  CONNECTIONS_COUNT,
+  MAX_CONNECTION_CONCURRENCY,
+  XRP_NODE_URLS,
+  EXPORT_TIMEOUT_MLS,
+  CONFIRMATIONS,
+  START_BLOCK,
+  LOOP_INTERVAL_CURRENT_MODE_SEC
+};
---
+++ blockchains/xrp/xrp_worker.js
@@ -0,0 +1,175 @@
+'use strict';
+const xrpl = require('xrpl');
+const assert = require('assert');
+const constants = require('./lib/constants');
+const { logger } = require('../../lib/logger');
+const BaseWorker = require('../../lib/worker_base');
+
+let PQueue = null;
+(async () => {
+  PQueue = (await import('p-queue')).default;
+})();
+
+class XRPWorker extends BaseWorker {
+  constructor() { 
+    super();
+    this.nodeURLs = constants.XRP_NODE_URLS.split(',');
+    this.connections = [];
+  }
+
+  async createNewSetConnections() {
+    if (this.nodeURLs.length === 0) {
+      throw 'Error: All API URLs returned error.';
+    }
+  
+    const nodeURL = this.nodeURLs.shift();
+    logger.info(`Using ${nodeURL} as XRPL API endpoint.`);
+    for (let i = 0; i < constants.CONNECTIONS_COUNT; i++) {
+      const clientOptions = { timeout: constants.DEFAULT_WS_TIMEOUT };
+      const api = new xrpl.Client(nodeURL, clientOptions);
+      await api.connect();
+
+      this.connections.push({
+        connection: api,
+        queue: new PQueue({ concurrency: constants.MAX_CONNECTION_CONCURRENCY }),
+        index: i
+      });
+    }
+  }
+
+  async connectionSend({connection, queue}, params) {
+    return queue.add(() => {
+      return connection.request(params);
+    });
+  }
+
+  async init() {
+    await this.createNewSetConnections();
+    const lastValidatedLedger = await this.connectionSend(this.connections[0], {
+      command: 'ledger',
+      ledger_index: 'validated',
+      transactions: true,
+      expand: false
+    });
+    const lastValidatedLedgerData = lastValidatedLedger.result;
+    this.lastConfirmedBlock = parseInt(lastValidatedLedgerData.ledger.ledger_index) - constants.CONFIRMATIONS;
+  }
+
+  async fetchLedgerTransactions(connection, ledger_index) {
+    const ledger = await this.connectionSend(connection, {
+      command: 'ledger',
+      ledger_index: parseInt(ledger_index),
+      transactions: true,
+      expand: false
+    }).then(value => value.result.ledger);
+
+    assert(ledger.closed === true);
+  
+    if (typeof ledger.transactions === 'undefined' || ledger.transactions.length === 0) {
+      return { ledger: ledger, transactions: [] };
+    }
+  
+    if (ledger.transactions.length > 200) {
+      logger.info(`<<< TOO MANY TXS at ledger ${ledger_index}: [[ ${ledger.transactions.length} ]], processing per-tx...`);
+      let transactions = ledger.transactions.map(tx =>
+        this.connectionSend(connection, {
+          command: 'tx',
+          transaction: tx,
+          minLedgerVersion: ledger_index,
+          maxLedgerVersion: ledger_index
+        }).catch((error) => {
+          if (error.message === 'txnNotFound') {
+            return Promise.resolve(null);
+          }
+  
+          return Promise.reject(error);
+        })
+      );
+  
+      transactions = await Promise.all(transactions);
+  
+      transactions = transactions.filter(t => t);
+
+      return { ledger: ledger, transactions };
+    }
+
+    const result = await this.connectionSend(connection, {
+      command: 'ledger',
+      ledger_index: parseInt(ledger_index),
+      transactions: true,
+      expand: true
+    }).then(value => value.result.ledger);
+  
+    assert(result.closed === true);
+  
+    return { ledger: ledger, transactions: result.transactions };
+  }
+
+  checkAllTransactionsValid(ledgers) {
+    for (let indexLedger = 0; indexLedger < ledgers.length; indexLedger++) {
+      const transactions = ledgers[indexLedger].transactions;
+      const blockNumber = ledgers[indexLedger].ledger.ledger_index;
+      for (let index = 0; index < transactions.length; index++) {
+        const transaction = transactions[index];
+        if('validated' in transaction && !transaction.validated) {
+          logger.error(`Transaction ${transaction.hash} at index ${index} in block ${blockNumber} is not validated. Aborting.`);
+          process.exit(-1);
+        }
+        if(!('meta' in transaction) && !('metaData' in transaction)) {
+          logger.error(`Transaction ${transaction.hash} at index ${index} in block ${blockNumber} is missing 'meta' field. Aborting.`);
+          process.exit(-1);
+        }
+      }
+    }
+  }
+
+  async work() {
+    if (this.lastConfirmedBlock === this.lastExportedBlock) {
+      this.sleepTimeMsec = constants.LOOP_INTERVAL_CURRENT_MODE_SEC * 1000;
+      const lastValidatedLedger = await this.connectionSend(this.connections[0], {
+        command: 'ledger',
+        ledger_index: 'validated',
+        transactions: true,
+        expand: false
+      });
+      const newConfirmedBlock = parseInt(lastValidatedLedger.result.ledger.ledger_index) - constants.CONFIRMATIONS;
+      if (newConfirmedBlock === this.lastConfirmedBlock) {
+        return [];
+      }
+      this.lastConfirmedBlock = newConfirmedBlock;
+    } else {
+      this.sleepTimeMsec = 0;
+    }
+    let toBlock = Math.min(this.lastExportedBlock + constants.SEND_BATCH_SIZE, this.lastConfirmedBlock);
+    let fromBlock = this.lastExportedBlock + 1;
+
+    const requests = [];
+    let transfers = [];
+    logger.info(`Fetching transfers for interval ${fromBlock}:${toBlock}`);
+    while (fromBlock + requests.length <= toBlock) {
+      const ledgerToDownload = fromBlock + requests.length;
+      requests.push(
+        await this.fetchLedgerTransactions(this.connections[ledgerToDownload % this.connections.length], ledgerToDownload)
+      );
+      if (requests.length >= constants.SEND_BATCH_SIZE || ledgerToDownload === toBlock) {
+        const ledgers = await Promise.all(requests).map(async ({ledger, transactions}) => {
+          return { ledger, transactions, primaryKey: ledger.ledger_index };
+        });
+        this.checkAllTransactionsValid(ledgers);
+
+        this.lastExportTime = Date.now();
+        this.lastExportedBlock = toBlock;
+
+        fromBlock += ledgers.length;
+        requests.length = 0;
+        transfers = transfers.concat(ledgers);
+      }
+    }
+
+    return transfers;
+  }
+}
+
+module.exports = {
+  worker: XRPWorker
+};
