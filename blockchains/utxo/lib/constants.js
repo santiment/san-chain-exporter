1f859304ca93b596bc10f2125888eaf7492ddf7d
LyudmilD <47607374+spiderjako@users.noreply.github.com>
2023-08-20 18:49:13 +0300

    Revert "Adding retry functionality"

--- blockchains/utxo/lib/constants.js
+++ blockchains/utxo/lib/constants.js
@@ -1,24 +1,21 @@
+const MAX_CONCURRENT_REQUESTS = parseInt(process.env.MAX_CONCURRENT_REQUESTS || '10');
+const DEFAULT_TIMEOUT = parseInt(process.env.DEFAULT_TIMEOUT || '10000');
+const CONFIRMATIONS = parseInt(process.env.CONFIRMATIONS || '3');
+const NODE_URL = process.env.NODE_URL || 'http://litecoin.stage.san:30992';
 const RPC_USERNAME = process.env.RPC_USERNAME || 'rpcuser';
 const RPC_PASSWORD = process.env.RPC_PASSWORD || 'rpcpassword';
-const NODE_URL = process.env.NODE_URL;
-
+const EXPORT_TIMEOUT_MLS = parseInt(process.env.EXPORT_TIMEOUT_MLS || 1000 * 60 * 15); // 15 minutes
+const LOOP_INTERVAL_CURRENT_MODE_SEC = parseInt(process.env.LOOP_INTERVAL_CURRENT_MODE_SEC || '30');
 const DOGE = process.env.DOGE || 0;
-const MAX_RETRIES = parseInt(process.env.MAX_RETRIES) || 3;
-const CONFIRMATIONS = parseInt(process.env.CONFIRMATIONS) || 3;
-const DEFAULT_TIMEOUT = parseInt(process.env.DEFAULT_TIMEOUT) || 10000;
-const MAX_CONCURRENT_REQUESTS = parseInt(process.env.MAX_CONCURRENT_REQUESTS) || 10;
-const EXPORT_TIMEOUT_MLS = parseInt(process.env.EXPORT_TIMEOUT_MLS) || 1000 * 60 * 15; // 15 minutes
-const LOOP_INTERVAL_CURRENT_MODE_SEC = parseInt(process.env.LOOP_INTERVAL_CURRENT_MODE_SEC) || 30;
 
 module.exports = {
-  DOGE,
-  NODE_URL,
-  MAX_RETRIES,
+  MAX_CONCURRENT_REQUESTS,
+  DEFAULT_TIMEOUT,
+  CONFIRMATIONS,
   RPC_USERNAME,
   RPC_PASSWORD,
-  CONFIRMATIONS,
-  DEFAULT_TIMEOUT,
+  NODE_URL,
   EXPORT_TIMEOUT_MLS,
-  MAX_CONCURRENT_REQUESTS,
+  DOGE,
   LOOP_INTERVAL_CURRENT_MODE_SEC
 };
--- blockchains/utxo/utxo_worker.js
+++ blockchains/utxo/utxo_worker.js
@@ -4,17 +4,15 @@ const { parseURL } = require('whatwg-url');
 const { logger } = require('../../lib/logger');
 const BaseWorker = require('../../lib/worker_base');
 const {
-  DOGE,
   NODE_URL,
-  MAX_RETRIES,
   RPC_PASSWORD,
   RPC_USERNAME,
-  CONFIRMATIONS,
   DEFAULT_TIMEOUT,
-  MAX_CONCURRENT_REQUESTS,
-  LOOP_INTERVAL_CURRENT_MODE_SEC
+  DOGE,
+  CONFIRMATIONS,
+  LOOP_INTERVAL_CURRENT_MODE_SEC,
+  MAX_CONCURRENT_REQUESTS
 } = require('./lib/constants');
-
 const URL = parseURL(NODE_URL);
 
 class UtxoWorker extends BaseWorker {
@@ -33,7 +31,7 @@ class UtxoWorker extends BaseWorker {
   }
 
   async init() {
-    const blockchainInfo = await this.sendRequestWithRetry('getblockchaininfo', []);
+    const blockchainInfo = await this.sendRequest('getblockchaininfo', []);
     this.lastConfirmedBlock = blockchainInfo.blocks - CONFIRMATIONS;
   }
 
@@ -47,40 +45,14 @@ class UtxoWorker extends BaseWorker {
     });
   }
 
-  async sendRequestWithRetry(method, params) {
-    let retries = 0;
-    let retryIntervalMs = 0;
-    while (retries < MAX_RETRIES) {
-      try {
-        const response = await this.sendRequest(method, params).catch(err => Promise.reject(err));
-        if (response.error || response.result === null) {
-          retries++;
-          retryIntervalMs += (2000 * retries);
-          logger.error(`sendRequest with ${method} failed. Reason: ${response.error}. Retrying for ${retries} time`);
-          await new Promise((resolve) => setTimeout(resolve, retryIntervalMs));
-          continue;
-        }
-        return response;
-      } catch(err) {
-        retries++;
-        retryIntervalMs += (2000 * retries);
-        logger.error(
-          `Try block in sendRequest for ${method} failed. Reason: ${err.toString()}. Waiting ${retryIntervalMs} and retrying for ${retries} time`
-          );
-        await new Promise((resolve) => setTimeout(resolve, retryIntervalMs));
-      }
-    }
-    return Promise.reject(`sendRequest for ${method} failed after ${MAX_RETRIES} retries`);
-  }
-
   async decodeTransaction(transaction_bytecode) {
-    return await this.sendRequestWithRetry('decoderawtransaction', [transaction_bytecode]);
+    return await this.sendRequest('decoderawtransaction', [transaction_bytecode]);
   }
 
   async getTransactionData(transaction_hashes) {
     const decodedTransactions = [];
     for (const transaction_hash of transaction_hashes) {
-      const transactionBytecode = await this.sendRequestWithRetry('getrawtransaction', [transaction_hash]);
+      const transactionBytecode = await this.sendRequest('getrawtransaction', [transaction_hash]);
       const decodedTransaction = await this.decodeTransaction(transactionBytecode);
       decodedTransactions.push(decodedTransaction);
     }
@@ -89,22 +61,22 @@ class UtxoWorker extends BaseWorker {
   }
 
   async fetchBlock(block_index) {
-    let blockHash = await this.sendRequestWithRetry('getblockhash', [block_index]);
+    let blockHash = await this.sendRequest('getblockhash', [block_index]);
     if (DOGE) {
-      let blockData = await this.sendRequestWithRetry('getblock', [blockHash, true]);
+      let blockData = await this.sendRequest('getblock', [blockHash, true]);
       let transactionData = await this.getTransactionData(blockData.tx);
       blockData['tx'] = transactionData;
 
       return blockData;
     }
-    return await this.sendRequestWithRetry('getblock', [blockHash, 2]);
+    return await this.sendRequest('getblock', [blockHash, 2]);
   }
 
   async work() {
     if (this.lastConfirmedBlock === this.lastExportedBlock) {
       this.sleepTimeMsec = LOOP_INTERVAL_CURRENT_MODE_SEC * 1000;
 
-      const blockchainInfo = await this.sendRequestWithRetry('getblockchaininfo', []);
+      const blockchainInfo = await this.sendRequest('getblockchaininfo', []);
       const newConfirmedBlock = blockchainInfo.blocks - CONFIRMATIONS;
       if (newConfirmedBlock === this.lastConfirmedBlock) {
         return [];
