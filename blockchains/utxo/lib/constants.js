5d7de99976711e0fd24c307620c7a1fa42ead1b7
Lyudmil Danailov <spiderjako@Lyudmils-MacBook-Pro.local>
2023-11-07 12:05:09 +0200

    Removing DOGE code

--- blockchains/utxo/lib/constants.js
+++ blockchains/utxo/lib/constants.js
@@ -2,7 +2,6 @@ const RPC_USERNAME = process.env.RPC_USERNAME || 'rpcuser';
 const RPC_PASSWORD = process.env.RPC_PASSWORD || 'rpcpassword';
 const NODE_URL = process.env.NODE_URL;
 
-const DOGE = process.env.DOGE || 0;
 const MAX_RETRIES = parseInt(process.env.MAX_RETRIES) || 3;
 const CONFIRMATIONS = parseInt(process.env.CONFIRMATIONS) || 3;
 const DEFAULT_TIMEOUT = parseInt(process.env.DEFAULT_TIMEOUT) || 10000;
@@ -11,7 +10,6 @@ const EXPORT_TIMEOUT_MLS = parseInt(process.env.EXPORT_TIMEOUT_MLS) || 1000 * 60
 const LOOP_INTERVAL_CURRENT_MODE_SEC = parseInt(process.env.LOOP_INTERVAL_CURRENT_MODE_SEC) || 30;
 
 module.exports = {
-  DOGE,
   NODE_URL,
   MAX_RETRIES,
   RPC_USERNAME,
