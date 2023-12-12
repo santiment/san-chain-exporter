6ca61108d8d367b4f0c826011bdbebfa23ff3da7
Yordan Pavlov <y.d.pavlov@gmail.com>
2023-12-12 12:12:24 +0200

    Logging added

--- blockchains/erc20/lib/fetch_events.js
+++ blockchains/erc20/lib/fetch_events.js
@@ -2,6 +2,7 @@
 
 const { decodeAddress } = require('./util');
 const { addCustomTokenDistribution } = require('./custom_token_distribution');
+const { logger } = require('../../../lib/logger');
 
 
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
@@ -181,7 +182,11 @@ const decodeFunctionsMap = {
 
 
 async function getPastEvents(web3Wrapper, fromBlock, toBlock, contractAddress, timestampsCache) {
+  const startTime = new Date();
+  logger.info('Sending request to node');
   const events = await getRawEvents(web3Wrapper, fromBlock, toBlock, contractAddress);
+  const endTime = new Date();
+  logger.info(`We got ${events.length} raw events from node for ${endTime - startTime} ms`);
   const decodedEvents = await decodeEvents(web3Wrapper, events, timestampsCache);
   const result = filterEvents(decodedEvents);
 
