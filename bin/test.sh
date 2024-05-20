b167bc0f38e5efcf6a7c322bf16b3b4624d437d2
Yordan Pavlov <y.d.pavlov@gmail.com>
2024-05-20 16:18:36 +0300

    Unify build type env variables

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 set -e
 
-docker build --build-arg NODE_ENV=development -t san-chain-exporter-test -f docker/Dockerfile .
+docker build --build-arg BUILD_ENV=development -t san-chain-exporter-test -f docker/Dockerfile .
 docker run --env BLOCKCHAIN=erc20 --rm -t san-chain-exporter-test npm test
