7b2537cf69563faae930b597c77d05ce61101140
Yordan Pavlov <y.d.pavlov@gmail.com>
2024-06-24 17:16:26 +0300

    ETH contracts working

--- src/blockchains/eth_contracts/eth_contracts_worker.ts
+++ src/blockchains/eth_contracts/eth_contracts_worker.ts
@@ -4,9 +4,11 @@ import { constructRPCClient } from '../../lib/http_client';
 import { BaseWorker } from '../../lib/worker_base';
 import { Web3Interface, constructWeb3Wrapper } from '../eth/lib/web3_wrapper';
 import { nextIntervalCalculator, analyzeWorkerContext, setWorkerSleepTime, NO_WORK_SLEEP } from '../eth/lib/next_interval_calculator';
-import { fetchEthInternalTrx, fetchBlocks } from '../eth/lib/fetch_data';
+import { fetchEthInternalTrx } from '../eth/lib/fetch_data';
 import { ETHBlock, Trace } from '../eth/eth_types';
 import { HTTPClientInterface } from '../../types';
+import { TimestampsCache } from '../erc20/lib/timestamps_cache';
+import { assertIsDefined } from '../../lib/utils';
 
 
 export class ETHContractsWorker extends BaseWorker {
@@ -47,90 +49,83 @@ export class ETHContractsWorker extends BaseWorker {
     return JSON.stringify(decodedBlock);
   }
 
-
-
-// Assuming locate and _address_creator are defined elsewhere
-function locate(array, predicate) {
-  return array.flatMap((element, index) => predicate(element) ? [index] : []);
-}
-
-function addressCreator(traces) {
-  // Add your implementation for `_address_creator` here.
-  // This is just a placeholder for the equivalent function from the Python code.
-  return null;
-}
-
   async work() {
-  const workerContext = await analyzeWorkerContext(this);
-  setWorkerSleepTime(this, workerContext);
-  if (workerContext === NO_WORK_SLEEP) return [];
+    const workerContext = await analyzeWorkerContext(this);
+    setWorkerSleepTime(this, workerContext);
+    if (workerContext === NO_WORK_SLEEP) return [];
 
-  const { fromBlock, toBlock } = nextIntervalCalculator(this);
-  logger.info(`Fetching blocks events for interval ${fromBlock}:${toBlock}`);
-  const blocks = await fetchBlocks(this.ethClient, this.web3Wrapper, fromBlock, toBlock);
-  const createFilter = { traceTypes: ['create'] }
-  const traces: Trace[] = await fetchEthInternalTrx(this.ethClient, this.web3Wrapper, fromBlock, toBlock, createFilter);
-  const grouped_traces = groupBy(traces, (tx: Trace) => tx.transactionHash);
-  grouped_traces = selectTracesWithCreateTrace(grouped_traces, blocks)
-  logging.info(f'Fetched {len(traces)} traces of which {len(traces_with_result)} have result')
-  const events = Array.from(blocks).map(([key, block]) => this.decodeBlock(block));
+    const { fromBlock, toBlock } = nextIntervalCalculator(this);
+    logger.info(`Fetching blocks events for interval ${fromBlock}:${toBlock}`);
+    const createFilter = { traceTypes: ['create'] }
+    const traces: Trace[] = await fetchEthInternalTrx(this.ethClient, this.web3Wrapper, fromBlock, toBlock, createFilter);
+    const groupedTraces = groupBy(traces, (tx: Trace) => tx.transactionHash);
+    const timestampsCache = new TimestampsCache(this.ethClient, this.web3Wrapper, fromBlock, toBlock);
+    const events = selectTracesWithCreateTrace(groupedTraces, timestampsCache)
 
-  this.lastExportedBlock = toBlock;
+    this.lastExportedBlock = toBlock;
 
-  return events;
-}
+    return events;
+  }
 
   async init() {
-  this.lastConfirmedBlock = await this.web3Wrapper.getBlockNumber() - this.settings.CONFIRMATIONS;
-}
-}
-
-type ContractCreation {
-  address: string,
-  address_fabric: string
-  address_creator: string,
-  bytecode
-}
-
-function selectTracesWithCreateTrace(groupedTraces: { [key: string]: Trace[] }, blockTimes: { [key: number]: number }) {
-  const txsWithCreateTrace: { [key: string], { [key: Trace[]], }
-} = { };
-
-for (const [tx, traces] of Object.entries(groupedTraces)) {
-  const traceTypes = traces.map(trace => trace.type);
-
-  const createTraceIndexes = traceTypes.flatMap((type, index) => type === 'create' ? [index] : []);
-
-  if (createTraceIndexes.length > 0) {
-    if (!txsWithCreateTrace.hasOwnProperty(tx)) {
-      txsWithCreateTrace[tx] = { traces: traces, records: [] };
-    }
-
-    for (const index of createTraceIndexes) {
-      const createTrace: Trace = traces[index];
-      let addressFabric;
-
-      if (createTrace.traceAddress.length > 0) {
-        addressFabric = traces[createTraceIndexes[createTraceIndexes.length - 1]].action.from;
-      } else {
-        addressFabric = null;
-      }
-
-      const record = {
-        address: createTrace.result.address,
-        address_fabric: addressFabric,
-        address_creator: addressCreator(traces),
-        bytecode: createTrace.result.code,
-        transaction_hash: createTrace.transactionHash,
-        block_number: createTrace.blockNumber,
-        block_created_at_timestamp: blockTimes[createTrace.blockNumber]
-      };
-
-      txsWithCreateTrace[tx].records.push(record);
-    }
+    this.lastConfirmedBlock = await this.web3Wrapper.getBlockNumber() - this.settings.CONFIRMATIONS;
   }
 }
 
-return txsWithCreateTrace;
+type ContractCreationTrace = {
+  address: string,
+  address_fabric?: string
+  address_creator: string,
+  bytecode: string,
+  transaction_hash: string,
+  block_number: number,
+  block_created_at_timestamp: number
+}
+
+type ContractCreationTracePerTx = {
+  [key: string]: ContractCreationTrace[]
+}
+
+function selectTracesWithCreateTrace(groupedTraces: { [key: string]: Trace[] }, blockTimes: TimestampsCache):
+  ContractCreationTrace[] {
+  const txsWithCreateTrace: ContractCreationTracePerTx = {};
+
+  for (const [tx, traces] of Object.entries(groupedTraces)) {
+    const traceTypes = traces.map(trace => trace.type);
+
+    const createTraceIndexes = traceTypes.flatMap((type, index) => type === 'create' ? [index] : []);
+
+    if (createTraceIndexes.length > 0) {
+      if (!txsWithCreateTrace.hasOwnProperty(tx)) {
+        txsWithCreateTrace[tx] = [];
+      }
+
+      for (const index of createTraceIndexes) {
+        const createTrace: Trace = traces[index];
+
+        assertIsDefined(createTrace.result.address, "'address' field is expected in trace result on 'create' type")
+        assertIsDefined(traces[0]['action']['from'], "'from' field shoud be set for first trace per tx")
+        assertIsDefined((createTrace.result as any).code, "'code' field should be set on create trace")
+
+
+        const record: ContractCreationTrace = {
+          address: createTrace.result.address,
+          address_creator: traces[0]['action']['from'],
+          bytecode: (createTrace.result as any).code,
+          transaction_hash: createTrace.transactionHash,
+          block_number: createTrace.blockNumber,
+          block_created_at_timestamp: blockTimes.getBlockTimestamp(createTrace.blockNumber)
+        };
+
+        if (createTrace.traceAddress.length > 0) {
+          record.address_fabric = traces[createTraceIndexes[createTraceIndexes.length - 1]].action.from;
+        }
+
+        txsWithCreateTrace[tx].push(record);
+      }
+    }
+  }
+
+  return Object.values(txsWithCreateTrace).flat();
 }
 
