42cb7cbfb1b341502cdb95a1cf810b8087d6618d
Yordan Pavlov <y.d.pavlov@gmail.com>
2024-07-09 15:44:34 +0300

    'Non empty' guard added to Zod type

--- src/blockchains/eth_contracts/lib/output_validator.ts
+++ src/blockchains/eth_contracts/lib/output_validator.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 const ContractCreationTrace = z.object({
-  address: z.string(),
+  address: z.string().min(1, "Address should be non empty"),
   address_fabric: z.string().optional(),
   address_creator: z.string(),
-  transaction_hash: z.string(),
+  transaction_hash: z.string().min(1, "Transaction hash should be non empty"),
   block_number: z.number().min(0, "Block number should not be negative"),
   block_created_at_timestamp: z.number().min(0, "Timestamp should not be negative"),
 });
