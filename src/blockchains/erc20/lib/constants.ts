3087ac3273db974947bbef9255793f595071ce69
Yordan Pavlov <y.d.pavlov@gmail.com>
2024-07-26 16:54:29 +0300

    Extend events with balances only after multicall contract deployment

--- src/blockchains/erc20/lib/constants.ts
+++ src/blockchains/erc20/lib/constants.ts
@@ -17,7 +17,7 @@ export const NODE_URL = process.env.NODE_URL || 'http://localhost:8545/';
 export const EVENTS_IN_SAME_PARTITION = process.env.EVENTS_IN_SAME_PARTITION || false;
 export const DEFAULT_TIMEOUT = getIntEnvVariable('DEFAULT_TIMEOUT', 10000);
 export const EXTEND_TRANSFERS_WITH_BALANCES = process.env.EXTEND_TRANSFERS_WITH_BALANCES || false;
-
+export const MULTICALL_DEPLOY_BLOCK = getIntEnvVariable('MULTICALL_DEPLOY_BLOCK', 14353601);
 
 export const CONTRACT_MAPPING_FILE_PATH = (
     process.env.CONTRACT_MAPPING_FILE_PATH ?
