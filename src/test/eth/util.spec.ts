9120a3e73c6c2685ffc7924974ec73e02b3a87a7
Yordan Pavlov <y.d.pavlov@gmail.com>
2024-12-19 16:38:32 +0200

    Unit tests of 'assignInternalTransactionPosition' added

--- src/test/eth/util.spec.ts
+++ src/test/eth/util.spec.ts
@@ -1,8 +1,9 @@
 import { expect } from 'earl';
-import { transactionOrder } from "../../blockchains/eth/lib/util"
+import { cloneDeep } from 'lodash';
+import { transactionOrder, assignInternalTransactionPosition } from "../../blockchains/eth/lib/util"
 import { ETHTransfer } from '../../blockchains/eth/eth_types';
 
-describe('utils', () => {
+describe('transactionOrder utils', () => {
     it('should sort by block number', () => {
         const transfers: ETHTransfer[] = [{
             from: "fromAddress",
@@ -90,5 +91,111 @@ describe('utils', () => {
         const result = transfers.sort(transactionOrder)
         expect(result[0].internalTransactionPosition).toEqual(2);
         expect(result[1].internalTransactionPosition).toEqual(5);
+    })
+})
+
+
+describe('assignInternalTransactionPosition utils', () => {
+    it('missing field is added', () => {
+        const transfers: ETHTransfer[] = [{
+            from: "fromAddress",
+            to: "toAddress",
+            value: 100,
+            valueExactBase36: "2S",
+            blockNumber: 2,
+            timestamp: 1000,
+            transactionHash: "hash",
+            transactionPosition: 10,
+            type: "type"
+        }]
+        const expected = cloneDeep(transfers)
+        expected[0].internalTransactionPosition = 0
+
+        const result = assignInternalTransactionPosition(transfers)
+
+        expect(result).toEqual(expected)
+    })
+
+    it('zero position is not changed', () => {
+        const transfers: ETHTransfer[] = [{
+            from: "fromAddress",
+            to: "toAddress",
+            value: 100,
+            valueExactBase36: "2S",
+            blockNumber: 2,
+            timestamp: 1000,
+            transactionHash: "hash",
+            transactionPosition: 10,
+            internalTransactionPosition: 0,
+            type: "type"
+        }]
+        const expected = cloneDeep(transfers)
+
+        const result = assignInternalTransactionPosition(transfers)
+        expect(result).toEqual(expected)
+    })
+
+    it('two different records assigned correctly', () => {
+        // Transaction hash is different even though they are in the same block. Expect not to get grouped.
+        const transfers: ETHTransfer[] = [{
+            from: "fromAddress",
+            to: "toAddress",
+            value: 100,
+            valueExactBase36: "2S",
+            blockNumber: 2,
+            timestamp: 1000,
+            transactionHash: "hash1",
+            transactionPosition: 1,
+            type: "type"
+        },
+        {
+            from: "fromAddress",
+            to: "toAddress",
+            value: 100,
+            valueExactBase36: "2S",
+            blockNumber: 2,
+            timestamp: 1000,
+            transactionHash: "hash2",
+            transactionPosition: 2,
+            type: "type"
+        }]
+        const expected = cloneDeep(transfers)
+        expected[0].internalTransactionPosition = 0
+        expected[1].internalTransactionPosition = 0
+
+        const result = assignInternalTransactionPosition(transfers)
+        expect(result).toEqual(expected)
+    })
+
+    it('two equal records assigned correctly', () => {
+        // Records are equal based on 'group by' criteria. Expect to get grouped.
+        const transfers: ETHTransfer[] = [{
+            from: "fromAddress",
+            to: "toAddress",
+            value: 100,
+            valueExactBase36: "2S",
+            blockNumber: 2,
+            timestamp: 1000,
+            transactionHash: "hash",
+            transactionPosition: 10,
+            type: "type"
+        },
+        {
+            from: "fromAddress",
+            to: "toAddress",
+            value: 50,
+            valueExactBase36: "1S",
+            blockNumber: 2,
+            timestamp: 1000,
+            transactionHash: "hash",
+            transactionPosition: 10,
+            type: "type"
+        }]
+        const expected = cloneDeep(transfers)
+        expected[0].internalTransactionPosition = 0
+        expected[1].internalTransactionPosition = 1
+
+        const result = assignInternalTransactionPosition(transfers)
+        expect(result).toEqual(expected)
     })
 })
