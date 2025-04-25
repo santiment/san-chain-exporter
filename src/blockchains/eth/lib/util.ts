988923433084555c51d9c4f2c8e4a92a86fd3488
Yordan Pavlov <y.d.pavlov@gmail.com>
2025-04-25 09:35:49 +0300

    Full ordering of internal tx position inside a tx
    * Assign incrementing internal tx position for all transfers within the
      same transaction. This creates full ordering within this transaction.
    * What we did before was, increment this internal transaction position
      counter only when multiple transfers are present for same 'from-to'.
    * The previous behavior does not give information on which transfer is
      to be handled first when one address interacts with more than one other
      addresses.

--- src/blockchains/eth/lib/util.ts
+++ src/blockchains/eth/lib/util.ts
@@ -23,7 +23,7 @@ export function transactionOrder(a: ETHTransfer | EOB, b: ETHTransfer | EOB) {
   return internalTxPositionA - internalTxPositionB
 }
 
-const ethTransferKey = (transfer: ETHTransfer) => `${transfer.blockNumber}-${transfer.transactionHash ?? ''}-${transfer.transactionPosition ?? ''}-${transfer.from}-${transfer.to}`
+const ethTransferKey = (transfer: ETHTransfer) => `${transfer.blockNumber}-${transfer.transactionHash ?? ''}`
 
 export function assignInternalTransactionPosition(transfers: ETHTransfer[], groupByKey: (transfer: ETHTransfer) => string = ethTransferKey): void {
   const grouped = groupBy(transfers, groupByKey)
